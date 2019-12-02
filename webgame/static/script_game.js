4216dd062ff16c72fc923f0e811d128634341c68
everlen <everlen.victor@gmail.com>
2019-12-02 20:15:48 -0300

    Commit criando a função sprite

--- webgame/static/script_game.js
+++ webgame/static/script_game.js
@@ -2,6 +2,38 @@ let inicio_canvas_x, inicio_canvas_y, canvas_altura, canvas_largura;
 let canvas, ctx;
 
 
+player = {
+
+    x, y, largura, altura,
+    imagem,    
+    
+    start : function(){
+        imagem = new Image();
+        imagem.src = "Img_game/PLAYER_SHIP_OFF.png";
+    },
+
+    desenhar : function(){
+
+        let sprite_player = new Sprite(this.imagem, largura, altura)
+        sprite_player.desenhar(this.x, this.y);
+    }
+};
+
+fundo = {
+    x = 0, y = 0, largura, altura,
+    imagem, sprite_fundo,
+
+    start : function(){
+        imagem = new Image();
+        imagem.src = "Img_game/fundo.png";
+        this.sprite_fundo = new Sprite(this.imagem, largura, altura)
+    },
+
+    desenhar : function(){        
+        this.sprite_fundo.desenhar(this.x, this.y);
+    }
+};
+
 
 function main(){ //Inicializa o jogo
 
@@ -18,7 +50,7 @@ function main(){ //Inicializa o jogo
     
     canvas.height = canvas_altura;
     canvas.width = canvas_largura;
-    canvas.style.border = "1px solid #000";
+    canvas.style.border = "1px solid #fff";
 
     ctx = canvas.getContext("2d");
 
@@ -26,6 +58,8 @@ function main(){ //Inicializa o jogo
     document.addEventListener("mousedown", click);
     document.addEventListener("touchstart", click);
 
+
+    fundo.start();
 
     update();
 }
@@ -44,12 +78,30 @@ function atualizar_acoes(){ //Atualizar as mudanças de informações de cada ob
 
 function desenhar(){ //Responsavel por chamar as funções que redesenham as informações atualizadas na tela
     contexto.fillStyle = "#50beff";
-    contexto.fillRect(inicio_canvas_x, inicio_canvas_y, canvas_largura, canvas_altura);
+    contexto.fillRect(0, 0, canvas_largura, canvas_altura);
+
+
+    fundo.desenhar();
 
 }
 
 function click(evento){
     //alert("clicou");
+
+}
+
+
+function Sprite(imagem, largura, altura){
+
+    this.imagem = imagem;
+    this.largura = largura;
+    this.altura = altura;
+
+    this.desenhar = function(xCanvas, yCanvas) {
+
+        ctx.drawImage(this.imagem, xCanvas, yCanvas);
+        
+    } 
 
 }
 
