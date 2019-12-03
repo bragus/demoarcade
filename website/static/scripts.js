b5cb04179b41f20a917c3d836b0481faa5134d2a
bragus <gusalmeidabraga@gmail.com>
2019-12-03 19:42:35 -0300

    Adicionado jQuery e iniciada a pagina home

---
+++ website/static/scripts.js
@@ -0,0 +1,24 @@
+$('#home_icon_play').mouseover(function(){
+    $('#mostrar_play').fadeIn("slow").css('display', 'block');
+  });
+  
+  $('#home_icon_play').mouseout(function(){
+    $('#mostrar_play').css('display', 'none');
+  });
+
+  $('#home_icon_about').mouseover(function(){
+    $('#mostrar_about').fadeIn("slow").css('display', 'block');
+  });
+  
+  $('#home_icon_about').mouseout(function(){
+    $('#mostrar_about').css('display', 'none');
+  });
+
+  
+  $('#home_icon_help').mouseover(function(){
+    $('#mostrar_help').fadeIn("slow").css('display', 'block');
+  });
+  
+  $('#home_icon_help').mouseout(function(){
+    $('#mostrar_help').css('display', 'none');
+  });
