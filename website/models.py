80b150b3acb7e02ced38d7bec96504663a3b20ed
JS-jopepe <jovenel.s@outlook.com>
2019-12-03 21:32:46 -0300

    Comecar a criar dados no models part2

--- website/models.py
+++ website/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class jogador():
+class dados_do_jogador(models.Model):
 
+    nome = models.CharField(max_length=20),
     nickname = models.CharField ( max_length = 10 ),
     idade = models.IntegerField (),
-    
+    email = models.EmailField(),
+
+    def __str__ (self):
+        return self.nickname
