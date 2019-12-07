9106d6db980c85a434fa1a123fa630d0de862259
bragus <gusalmeidabraga@gmail.com>
2019-12-07 12:04:22 -0300

    criando páginas sobre e ranking

--- website/views.py
+++ website/views.py
@@ -5,5 +5,11 @@ from django.shortcuts import render
 def Mostrar_home (request):
     return render (request, 'home.html')
 
+def Mostrar_sobre (request):
+    return render (request, 'sobre.html')
+
 def Mostrar_instrucoes (request):
     return render (request, 'instrucoes.html')
+
+def Mostrar_ranking (request):
+    return render (request, 'ranking.html')
