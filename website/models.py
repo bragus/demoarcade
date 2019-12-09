1492ab8c8e111963b745376b010985bad25ec174
JS-jopepe <jovenel.s@outlook.com>
2019-12-09 20:38:00 -0300

    Finalizando o Models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class player(models.Model):
+    nome = models.CharField(max_length=25)
+    nickname = models.CharField(max_length=10)
+    senha = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nickname
+
+class pontuacao(models.Model):
+    player = models.ForeignKey(player, on_delete=models.CASCADE)
+    pontuacao = models.IntegerField()
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from website.models import *
 
 # Create your views here.
 
