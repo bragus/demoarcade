9b4997851b87fe7f5e484250ba923d6193d96323
Everlen <victor.everlen@gmail.com>
2019-12-09 15:50:46 -0300

    Desenvolvimento das mecanicas de meteoros, Função de detecção de colisão, Exibição da pontuação da partida

--- webgame/static/script_game.js
+++ webgame/static/script_game.js
@@ -12,13 +12,13 @@ player = {
     largura: 50,
     cor: "#ff4e4e",
     imagem: null,    
-
+    vida: 0,
     velocidade: 6,
-    score: 0,
     
     start : function(){
 
         this.y = canvas_altura/2;
+        this.vida = 6;
 
         //imagem = new Image();
         //imagem.src = "Img_game/PLAYER_SHIP_OFF.png";
@@ -89,8 +89,249 @@ player = {
         ctx.fillStyle = this.cor;
         ctx.fillRect(this.x, this.y, this.altura, this.largura);
 
+    },
+
+    atingido : function(){
+
+        this.vida--;
+        if(this.vida <= 0){ // perdeu o game
+            alert("Perdeu");
+        }        
+
     }
 };
+
+
+C_Tiros = { //controlador de tiros do player
+
+    listTiros: [],
+    cores: ["#ffbc1c", "#ff1c1c", "#ff85e1"],
+    tempo_inserir: 30,
+    config_tempo_inserir: 10,
+    velocidade_tiro: 10,
+    atirar: false,
+
+
+    insere: function(){
+        this.listTiros.push({
+            x: player.x + player.largura,
+            y: player.y + player.altura/2,
+            largura: 20,
+            altura: 4,
+            cor: this.cores[Math.floor(3*Math.random())],
+        });
+
+        this.tempo_inserir = this.config_tempo_inserir;
+    },
+
+
+    update: function(){
+
+        if(this.atirar){
+            if(this.tempo_inserir <= 0){
+                this.insere();
+            }
+            else{
+                this.tempo_inserir--;
+            }
+        }
+
+        for(let i = 0; i < this.listTiros.length; i++){
+
+            let obj_tiro = this.listTiros[i];
+            obj_tiro.x += this.velocidade_tiro;
+
+        }
+
+    },
+
+    desenhar: function(){
+
+        for(let i = 0; i < this.listTiros.length; i++){
+
+            let obj_tiro = this.listTiros[i];
+            ctx.fillStyle = obj_tiro.cor;
+            ctx.fillRect(obj_tiro.x, obj_tiro.y, obj_tiro.largura, obj_tiro.altura);
+
+        }
+
+    }
+
+};
+
+C_Asteroides = {
+
+    listAst: [],
+    cores: ["#ffbc1c", "#ff1c1c", "#ff85e1"],
+    pos_y: [1, 2, 3], //1 = asteroide vindo de cima para baixo, 2 = vindo do meio, 3 = de baixo para cima
+    tempo_inserir: 30,
+    config_tempo_inserir: 20,
+
+    insere: function(){
+
+        pos_y = this.pos_y[Math.floor(3*Math.random())];
+        pos_x = 0;
+        velocidade_x = 0;
+        velocidade_y = 0;
+
+        if(pos_y == 1){ // vindo de cima
+            pos_y = 0;
+            pos_x = canvas_largura/1.8 + Math.floor((canvas_largura/2.2) * Math.random());
+
+            velocidade_x = - (5 + Math.floor(4 * Math.random()));
+            velocidade_y = (2 + Math.floor(4 * Math.random()));            
+        }
+        else if (pos_y == 2){ // vindo do centro
+            pos_y = Math.floor(canvas_altura * Math.random());
+            pos_x = canvas_largura;
+
+            velocidade_x = - (5 + Math.floor(4 * Math.random()));
+
+            if(Math.floor(2 * Math.random()) == 1) // sorteia se sera de baixo para cima ou de cima para baixo
+                velocidade_y = (1 + Math.floor(2 * Math.random()));
+            else
+                velocidade_y = - (1 + Math.floor(2 * Math.random()));
+        }
+        else{ // vindo de baixo
+            pos_y = canvas_altura;
+            pos_x = canvas_largura/1.8 + Math.floor((canvas_largura/2.2) * Math.random());
+
+            velocidade_x = - (5 + Math.floor(4 * Math.random()));
+            velocidade_y = - (2 + Math.floor(4 * Math.random()));
+        }
+
+        this.listAst.push({
+            x: pos_x,
+            y: pos_y,
+            largura: 30,
+            altura: 30,
+            vel_x: velocidade_x,
+            vel_y: velocidade_y,
+
+            cor: this.cores[Math.floor(3*Math.random())],
+            vida: 3,
+        });
+
+        score.adicionar_pontuacao(5);
+
+        this.tempo_inserir = this.config_tempo_inserir;
+    },
+
+
+    update: function(){
+
+        if(this.tempo_inserir <= 0){
+            this.insere();
+        }
+        else{
+            this.tempo_inserir--;
+        }
+
+        for(let i = 0; i < this.listAst.length; i++){
+
+            let obj_ast = this.listAst[i];
+
+            for(let c = 0; c < C_Tiros.listTiros.length; c++){
+
+                let obj_tiro = C_Tiros.listTiros[c];
+
+                if(detectar_colisao(obj_ast, obj_tiro)){ // detecta colisao de tiros com asteroides
+                    
+                    obj_ast.vida--;
+
+                    if(obj_ast.vida == 0){ //asteroide morreu
+                                                
+                        this.listAst.splice(i, 1); //Remove os itens dentro do intervalo, coloque 1 se for apenas um item
+                        i--;
+                        this.destruir(obj_ast, false);
+                                         
+                    }
+
+                }
+
+            }
+            
+            if(detectar_colisao(obj_ast, player)){ // detecta colisão de asteroides com o player
+             
+                this.listAst.splice(i, 1); 
+                i--;
+                this.destruir(obj_ast, true);
+                player.atingido();
+
+            }
+
+            obj_ast.x += obj_ast.vel_x;
+            obj_ast.y += obj_ast.vel_y;
+
+        }
+
+    },
+
+    desenhar: function(){
+
+        for(let i = 0; i < this.listAst.length; i++){
+
+            let obj_ast = this.listAst[i];
+            ctx.fillStyle = obj_ast.cor;
+            ctx.fillRect(obj_ast.x, obj_ast.y, obj_ast.largura, obj_ast.altura);
+
+        }
+
+    },
+
+    destruir: function(ast, atingiu_player){
+
+        if(!atingiu_player)
+            score.adicionar_pontuacao(50);
+
+    },
+
+};
+
+score = {
+
+    x: 0,
+    y: 0,
+    altura: 50,
+    largura: 50,
+    cor: "#ff4e4e",
+    valorScore: 0,
+
+    start : function(){
+        this.x = 20;
+        this.y = canvas_altura - 50;
+    },
+
+    update : function(){
+        
+
+        
+    },
+
+    desenhar : function(){
+
+        ctx.fillStyle = this.cor;
+        ctx.font = "50px Arial";
+        ctx.fillText(this.valorScore, this.x, this.y);
+
+    },
+
+    adicionar_pontuacao : function(valor){
+
+        this.valorScore += valor;
+    },
+};
+
+function detectar_colisao(obj1, obj2){
+
+    if((obj1.x + obj1.largura) >= obj2.x)
+        if(obj1.x <= (obj2.x + obj2.largura))
+            if((obj1.y + obj1.altura) >= obj2.y)
+                if(obj1.y <= (obj2.y + obj2.altura))
+                    return true;
+
+    return false;
+}
 
 // fundo = {
 //     // x = 0, y = 0, largura, altura,
@@ -133,12 +374,15 @@ function main(){ //Inicializa o jogo
 
     document.body.appendChild(canvas);
     document.addEventListener("mousedown", click);
+    document.addEventListener("mouseup", clickEnd);
     document.addEventListener("touchstart", click);
+    document.addEventListener("touchend", clickEnd);
     window.addEventListener("mousemove", getMousePos);
 
     // fundo.start();
 
     player.start();
+    score.start();
 
     update();
 }
@@ -156,6 +400,8 @@ function atualizar_acoes(){ //Atualizar as mudanças de informações de cada ob
     
     //let rect = canvas.getBoundingClientRect();
     player.update();
+    C_Tiros.update();
+    C_Asteroides.update();
 
 }
 
@@ -166,6 +412,9 @@ function desenhar(){ //Responsavel por chamar as funções que redesenham as inf
 
     //fundo.desenhar();
     player.desenhar();
+    score.desenhar();
+    C_Tiros.desenhar();
+    C_Asteroides.desenhar();
 }
 
 
@@ -176,7 +425,14 @@ function getMousePos(evt) {
 
 
 function click(evento){
-    //alert("clicou");
+
+    C_Tiros.atirar = true;
+
+}
+
+function clickEnd(evento){
+
+    C_Tiros.atirar = false;
 
 }
 
