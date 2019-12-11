9a2cd1e84df38a4934b412bf2d8c4190cb782c7b
Everlen <victor.everlen@gmail.com>
2019-12-11 04:21:28 -0300

    Tentativa de realizar o login de usuario 1

--- website/views.py
+++ website/views.py
@@ -1,20 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from website.forms import playerForm
 from website.models import *
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def Mostrar_home (request):
     form = playerForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        context = {
-            'msg' : "Você foi cadastrado com sucesso!"        
-        }
-        return render (request, 'home.html', context)
+
+    if request.POST:
+        if form.is_valid(): #Cadastro
+            nickname = form.cleaned_data['nickname']
+            senha = form.cleaned_data['senha']            
+
+            #Buscar correspondencia no banco          
+            user = authenticate(nickname = nickname, senha = senha)
+            if user is not None:
+                context = {
+                    'msg' : "Você foi logado com sucesso!"        
+                }
+                return redirect("/game")  
+
+            else:
+                form.save()
+                context = {
+                    'msg' : "Você foi cadastrado com sucesso!"        
+                }
+                return redirect("/game")      
+            
+
     context = {
         'formulary':form
     }
+
     return render (request, 'home.html', context)
 
 def Mostrar_sobre (request):
